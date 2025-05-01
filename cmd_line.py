46725fc4d534e3943b2cdb94054395bf76f6a9c2
David Zhan <zhandavidz@gmail.com>
2025-05-01 17:58:22 -0400

    updated cmd line stuff

--- cmd_line.py
+++ cmd_line.py
@@ -1,4 +1,5 @@
 import argparse
+from typing import Optional, Callable
 
 def parse_command(command):
     """Parses a command string into a list of arguments."""
@@ -6,18 +7,23 @@ def parse_command(command):
     parser.add_argument('args', nargs=argparse.REMAINDER, help="Command arguments")
     return parser.parse_args(command.split()).args
 
-def command_prompt():
-    """Starts the command prompt loop."""
+def command_prompt(get_prefix: Optional[Callable] = None, parser: Optional[argparse.ArgumentParser] = None):
+    exit_cmds  = {"exit", "quit"}
+    exit_msg = "\nLeaving CatFS, goodbye!"
     while True:
         try:
-            user_input = input("catfs 🐱 ")
-            if user_input.strip().lower() in {"exit", "quit"}:
-                print("Exiting command prompt. Goodbye!")
+            prefix = f"{get_prefix()} " if get_prefix else ""
+            user_input = input(f"{prefix}🐱  ")
+            user_input = user_input.strip()
+            if not user_input:
+                continue
+            if user_input.strip().lower() in exit_cmds:
+                print(exit_msg)
                 break
-            parsed_input = parse_command(user_input)
+            parsed_input = parser.parse_args(user_input.split()) if parser else user_input.split()
             print(f"Parsed command: {parsed_input}")
         except KeyboardInterrupt:
-            print("\nExiting command prompt. Goodbye!")
+            print(exit_msg)
             break
         except Exception as e:
             print(f"Error: {e}")
--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from cmd_line import command_prompt
+import argparse
+
+cur_dir = "/"
+
+def get_dir():
+    return cur_dir
 
 def main():
-    command_prompt()
+
+    parser = argparse.ArgumentParser(description="CatFS command parser")
+    parser.add_argument("ls", nargs="*", help="List the cats and rooms in the current room")
+
+
+
+    command_prompt(get_prefix=get_dir)
 
 if __name__ == "__main__":
     main()
